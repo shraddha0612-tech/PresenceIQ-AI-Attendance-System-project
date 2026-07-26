d91b32f629d819476a932a0f5d12f36c0cb56c3e
shraddha mishra <shubhimishraa06@gmail.com>
2026-07-26 17:52:02 +0530

    finalizing touches

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-from src.UI.base_layout import style_base_layout
-from src.UI.base_layout import style_background_dashboard
+from src.ui.base_layout import style_base_layout
+from src.ui.base_layout import style_background_dashboard
 from src.components.header import header_dashboard
 from src.database.db import check_teacher_exist,create_teacher, teacher_login,get_teacher_subjects, get_attendance_for_teacher
 from src.components.dialog_create_subject import create_subject_dialog
@@ -9,7 +9,7 @@ from src.components.subject_card import subject_card
 from src.components.dialog_share_subject import share_subject_dialog
 from src.components.dialog_add_photo import add_photos_dialog
 from src.pipelines.face_pipeline import predict_attendance
-from src.components.diag_attendance_results import attendance_result_dialog
+from src.components.dialog_attendance_results import attendance_result_dialog
 import numpy as np
 from src.database.config import supabase
 from datetime import datetime
